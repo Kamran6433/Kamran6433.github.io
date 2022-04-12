45b75cc010d41363528d9ac7a91f2d6e55c4ec0b
kamran6433 <kamran_12@icloud.com>
2022-04-12 14:09:48 +0100

    working on the footer and not using percentages anymore.

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-
-
-let name = "Kamran"; //string
-let gay = false; //boolean
-let number = 67; //number
-
 //accepting user input
 // let username = window.prompt("what is your name?");
 // console.log(username);
-
 
 var background = ["../Kamran6433.github.io/Images/NCLCampus2.jpg",
     "../Kamran6433.github.io/Images/NCLCampus.jpg",
@@ -27,17 +20,52 @@ function changeImg() {
 }
 window.onload = changeImg;
 
-let instagramAccount;
-let firstName;
-let lastName;
-let studentID;
-document.getElementById("submit").onclick = function () {
-    instagramAccount = document.getElementById("instagram_account").value;
-    firstName = document.getElementById("first_name").value;
-    lastName = document.getElementById("last_name").value;
-    studentID = document.getElementById("student_id").value;
+var aosElements = Array.from(document.querySelectorAll('.aos'));
+console.log(aosElements);
+
+window.addEventListener('scroll', throttle(scanElements, 50))
+
+function scanElements(e) {
+    console.count(e);
+    aosElements.forEach(element => {
+        if(isVisable(element)) {
+            element.classList.add('active');
+        } else {
+            element.classList.remove('active');
+        }
+    })
 }
-console.log(instagramAccount, firstName, lastName, studentID);
+
+function isVisable(element) {
+    const elementDiv = element.getBoundingClientRect();
+    let distanceFromTop = -100;
+    return elementDiv.top - window.innerHeight < distanceFromTop ? true : false;
+}
+
+function throttle(fn, delay) {
+    let lastCall = 0;
+    return (...args) => {
+        let context = this;
+        let current = new Date().getTime();
+        if (current - lastCall < delay) {
+            return
+        }
+        lastCall = current;
+        return fn.apply(context, ...args);
+    }
+}
+
+// let instagramAccount;
+// let firstName;
+// let lastName;
+// let studentID;
+// document.getElementById("submit").onclick = function () {
+//     instagramAccount = document.getElementById("instagram_account").value;
+//     firstName = document.getElementById("first_name").value;
+//     lastName = document.getElementById("last_name").value;
+//     studentID = document.getElementById("student_id").value;
+// }
+// console.log(instagramAccount, firstName, lastName, studentID);
 
 
 
