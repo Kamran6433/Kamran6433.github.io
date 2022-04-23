ff3a3e5d7aafd54c641a66783b9bc7041253b795
kamran6433 <kamran_12@icloud.com>
2022-04-23 14:50:54 +0100

    Added a popup feature for students to report any misconduct.

--- index.js
+++ index.js
@@ -78,3 +78,40 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.act');
+    modals.forEach(modal => {
+        closeModal(modal);
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('act');
+    overlay.classList.add('act');
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('act');
+    overlay.classList.remove('act');
+}
