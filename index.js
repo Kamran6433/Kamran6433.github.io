941facc6ede5a70e86bcee477ecc766ecddb28b3
kamran6433 <kamran_12@icloud.com>
2022-05-02 12:58:45 +0100

    tweaks.

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function changeImg() {
     background[i].classList.add("showing");
     setTimeout("changeImg()", 5000);
 }
-// setTimeout("changeImg()", 5000);
 window.onload = changeImg;
 
 
@@ -49,6 +48,60 @@ function throttle(fn, delay) {
         return fn.apply(context, ...args);
     }
 }
+
+var slides = document.querySelectorAll('.containSlider');
+var btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+
+//Javascript for the image slider manual navigation
+var manualNav = function (manual) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        btns.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+    });
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+    });
+});
+
+//JavaScript for image slider autoplay navigation
+var repeat = function(activeClass) {
+    let active = document.getElementsByClassName('active');
+    let i =1;
+
+    var repeater = () => {
+        setTimeout(function() {
+            [...active].forEach((activeSlide) => {
+                activeSlide.classList.remove('active');
+            });
+
+
+            slides[i].classList.add('active');
+            btns[i].classList.add('active');
+            i++;
+
+            if(slides.length == i) {
+                i = 0;
+            }
+            if(i >= slides.length) {
+                return;
+            }
+            repeater();
+        }, 1000);
+    }
+    repeater();
+}
+repeat();
 
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 70,
