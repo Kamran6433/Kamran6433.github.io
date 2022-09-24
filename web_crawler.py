23f4ed14816fdf23a1bc42e9c5c3b8ae2a58d9c9
kamran6433 <kamran_12@icloud.com>
2022-09-24 17:33:56 +0100

    Updated the link to the NUSU Page.

--- web_crawler.py
+++ web_crawler.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 
 url = requests.get('https://www.instagram.com/nclpaksoc/')
+# print(url.text)
 soup = BeautifulSoup(url.content, 'html.parser')
-instagram_id = soup.find('div', class_='_ab8w  _ab94 _ab99 _ab9f _ab9m _ab9o _abcm')
-print(instagram_id)
+print(soup.prettify())
+instagram_id = soup.find('div', class_="_aabd _aa8k _aanf")
+print(instagram_id.a)
